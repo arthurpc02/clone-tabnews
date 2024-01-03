0761cf8b2174fc3bcca83654542d7373c508054b
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-01-03 19:46:48 +0000

    adds remaining files of the project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    First_Header();
+    return <h1>***Aqui eu criando a página!***</h1>
+}
+
+function First_Header() {
+    return <h2>outro header</h2>
+}
+
+export default Home;
