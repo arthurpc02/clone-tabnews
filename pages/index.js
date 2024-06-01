e3b48cdc0086251cf6196a5867e7a09137fcfe7f
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-06-01 01:23:43 +0000

    Add an animation to the page.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
 function Home() {
-  First_Header();
-  return <h1>***Aqui eu criando a página!***</h1>;
-}
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100dvw",
+        height: "100dvh",
+        overflowX: "hidden",
+      }}
+    >
+      <h1>*Me creating the webpage*</h1>
+      <script
+        src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+        type="module"
+      ></script>
 
-function First_Header() {
-  return <h2>outro header</h2>;
+      <dotlottie-player
+        src="https://lottie.host/026e2865-4ae8-441b-919a-b4147e9f7896/sV9R0R69qk.json"
+        background="transparent"
+        speed="1"
+        style={{ width: "300px", height: "300px" }}
+        loop
+        autoplay
+      ></dotlottie-player>
+    </main>
+  );
 }
 
 export default Home;
