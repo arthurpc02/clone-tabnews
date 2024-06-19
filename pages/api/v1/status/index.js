70333dc726b2c7980606ddc1f0e77dff4c3f634c
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-06-19 01:15:03 +0000

    adds new propery to /status endpoint: activeConnections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,13 +3,27 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
+  const postgresVersion = null;
+
   var maxConnectionsQuery = new Object();
-  maxConnectionsQuery = await database.query("SHOW max_connections;");
+  maxConnectionsQuery = await database.query("SHOW max_connections");
   const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
+  var activeConnectionsQuery = new Object();
+  activeConnectionsQuery = await database.query(
+    "SELECT COUNT(DISTINCT pid) FROM pg_stat_activity WHERE state='active';",
+  );
+  const activeConnections = activeConnectionsQuery.rows[0].count;
+  // QUERY 2: SELECT version()
+  // QUERY 3: SELECT COUNT(DISTINCT pid) FROM pg_stat_activity WHERE state='active';
 
   response.status(200).json({
     updated_at: updatedAt,
-    max_connections: maxConnections,
+    database_stats: {
+      postgres_version: postgresVersion,
+      max_connections: maxConnections,
+      active_connections: activeConnections,
+    },
   });
 }
 
