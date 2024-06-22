b576e0bf4389f0dace64d3f027c834bb782deb2f
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-06-22 17:28:27 +0000

    adds test to database_stats in the /status

--- tests/integration/integration/api/v1/status/get.test.js
+++ tests/integration/integration/api/v1/status/get.test.js
@@ -7,4 +7,19 @@ test("Get to /api/v1/status should return 200", async () => {
 
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(parsedUpdateAt);
+
+  const postgres_version = responseBody.database_stats.postgres_version;
+  expect(postgres_version).toBeDefined();
+
+  const max_connections = responseBody.database_stats.max_connections;
+  expect(max_connections).toBeDefined();
+  expect(max_connections).not.toBe(null);
+
+  const active_connections = responseBody.database_stats.active_connections;
+  expect(active_connections).toBeDefined();
+  expect(active_connections).not.toBe(null);
 });
+
+function isNumber(value) {
+  return typeof value === "number";
+}
