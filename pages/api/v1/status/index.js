8b902922b7df91464df801657b3101b90b982e22
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-06-22 17:00:39 +0000

    adds postgres version to the /status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,9 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const postgresVersion = null;
+  var postgresVersionQuery = new Object();
+  postgresVersionQuery = await database.query("SHOW server_version");
+  const postgresVersion = postgresVersionQuery.rows[0].server_version;
 
   var maxConnectionsQuery = new Object();
   maxConnectionsQuery = await database.query("SHOW max_connections");
@@ -14,8 +16,7 @@ async function status(request, response) {
     "SELECT COUNT(DISTINCT pid) FROM pg_stat_activity WHERE state='active';",
   );
   const activeConnections = activeConnectionsQuery.rows[0].count;
-  // QUERY 2: SELECT version()
-  // QUERY 3: SELECT COUNT(DISTINCT pid) FROM pg_stat_activity WHERE state='active';
+  // QUERY 2: SHOW server_version;
 
   response.status(200).json({
     updated_at: updatedAt,
