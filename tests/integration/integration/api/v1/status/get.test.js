897561a91dc8470aef54625018cdf3726b977170
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-06-22 18:37:26 +0000

    removes unnecessary test, and assigns defined version for the database test

--- tests/integration/integration/api/v1/status/get.test.js
+++ tests/integration/integration/api/v1/status/get.test.js
@@ -3,13 +3,12 @@ test("Get to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(parsedUpdateAt);
 
   const postgres_version = responseBody.database_stats.postgres_version;
-  expect(postgres_version).toBeDefined();
+  expect(postgres_version).toEqual("16.0");
 
   const max_connections = responseBody.database_stats.max_connections;
   expect(max_connections).toBeDefined();
