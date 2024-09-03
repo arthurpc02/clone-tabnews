3feefcf48474c02b9b9ba8d1aa9a4e6ba41fd9f7
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-09-03 01:07:34 +0000

    implementing post for the endpoint ´/migrations´.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,16 +1,30 @@
-import migrationRunner from 'node-pg-migrate'
-import { join } from "node:path"
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json(migrations);
+  } else if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json(migrations);
+  } else {
+    console.log("Method is neither GET nor POST.");
+    return response.status(405).end;
+  }
+  return response.status(405).end;
 }
-
-
---
+++ tests/integration/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,11 @@
+test("Post to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+});
