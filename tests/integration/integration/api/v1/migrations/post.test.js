439168c007cb00af101f80aa7640da9f3c7a1b39
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-09-07 13:50:11 +0000

    improving the POST migration to test.

--- tests/integration/integration/api/v1/migrations/post.test.js
+++ tests/integration/integration/api/v1/migrations/post.test.js
@@ -1,19 +1,33 @@
-import database from 'infra/database.js';
+import database from "infra/database.js";
 
 beforeAll(cleanDatabase);
 
-async function cleanDatabase(){
+async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public");
 }
 
 test("Post to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(200);
 
-  const responseBody = await response.json();
-  console.log(responseBody);
+  const response1Body = await response1.json();
+  console.log(response1Body);
 
-  expect(Array.isArray(responseBody)).toBe(true);
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  // another post, this time we don't expect the migrations to excute because they
+  // were already executed in response1.
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+  console.log(response2Body);
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
