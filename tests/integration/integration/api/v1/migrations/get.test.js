7f25be447fbb693c006db83fab19281e7eaaf4b7
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-09-07 13:23:50 +0000

    added cleanDatabase() in my tests.

--- tests/integration/integration/api/v1/migrations/get.test.js
+++ tests/integration/integration/api/v1/migrations/get.test.js
@@ -1,6 +1,10 @@
 import database from 'infra/database.js';
 
-database.query("SELECT 1+1;");
+beforeAll(cleanDatabase);
+
+async function cleanDatabase(){
+  await database.query("drop schema public cascade; create schema public");
+}
 
 test("Get to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
--- tests/integration/integration/api/v1/migrations/post.test.js
+++ tests/integration/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from 'infra/database.js';
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase(){
+  await database.query("drop schema public cascade; create schema public");
+}
+
 test("Post to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
