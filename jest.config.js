a5bccabdfec14a8588d967eb4e4ccf68dd6268f4
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-09-07 12:04:51 +0000

    i was able to get the env variables working with chat gpt's help.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
 // this file uses old commonJS import methods because Jest does not support ESM.
-const nextJest = require('next/jest');
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: '.env.test'
+});
 
 const createJestConfig = nextJest();
 const jestConfig = createJestConfig({
-  moduleDirectories: ['node_modules', "<rootDir>"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
 });
 
 console.log("NODE_ENV = " + process.env.NODE_ENV);
+console.log("pg host = " + process.env.POSTGRES_HOST);
+
 
 module.exports = jestConfig;
--- tests/integration/integration/api/v1/migrations/get.test.js
+++ tests/integration/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,7 @@
 import database from 'infra/database.js';
 
-// console.log("Ambiente: " + process.env.POSTGRES_HOST);
-// console.log("Ambiente: " + process.env.NODE_ENV);
+console.log("PG host: " + process.env.POSTGRES_HOST);
+
 test("Get to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
