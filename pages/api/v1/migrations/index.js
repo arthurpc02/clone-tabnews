d04fe646a8b9f6508c35b08556abccfb995ae769
arthurpc02 <35460423+arthurpc02@users.noreply.github.com>
2024-09-07 22:37:35 +0000

    connecting the database in function getNewClient()

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,11 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -13,12 +15,15 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    response.status(200).json(pendingMigrations);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   } else if (request.method === "POST") {
     const migratedMigrations = await migrationRunner({
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
@@ -29,5 +34,4 @@ export default async function migrations(request, response) {
     console.log("Method is neither GET nor POST.");
     return response.status(405).end;
   }
-  return response.status(405).end;
 }
