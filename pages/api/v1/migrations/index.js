36f7fb6474a0c0a22774c4a3fc2586423ce0ee90
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2025-01-28 01:22:01 +0000

    fix bug where open connections hang if the migrations method is not GET or POST

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,35 +3,42 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedRequests = new Set(["GET", "POST"]);
+  if(allowedRequests.has(request.method))
+  {
+    const dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  } else if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
+    } else if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+  
+      await dbClient.end();
+  
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+  
+      return response.status(200).json(migratedMigrations);
+    } else {
+      console.log("Method not allowed");
     }
-
-    return response.status(200).json(migratedMigrations);
-  } else {
-    console.log("Method is neither GET nor POST.");
-    return response.status(405).end;
+  }
+  else {
+    console.log("Method not allowed.");
+    return response.status(405).end();
   }
 }
---
+++ tests/integration/integration/api/v1/migrations/other_requests.test.js
@@ -0,0 +1,29 @@
+import database from "infra/database.js";
+
+
+test("Post to /api/v1/migrations should return 201", async () => {
+  console.log("Test desc.: Post to /api/v1/migrations with any method beside GET & POST should return 201");
+
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  expect(response1.status).toBe(405);
+
+  // const response1Body = await response1.json();
+  // console.log(response1Body);
+
+  // expect(Array.isArray(response1Body)).toBe(true);
+  // expect(response2Body.length).toBe(0);
+
+  // // another request, different method
+  // const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+  //   method: "PUT",
+  // });
+  // expect(response2.status).toBe(200);
+
+  // const response2Body = await response2.json();
+  // console.log(response2Body);
+
+  // expect(Array.isArray(response2Body)).toBe(true);
+  // expect(response2Body.length).toBe(0);
+});
