ec586dd3344681fe0fb3b09a8932d495af37d986
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2025-02-06 00:03:39 +0000

    optimize npm test execution with maxTimeout.

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,9 +6,11 @@ async function waitForAllServices(){
   async function waitForWebServer(){
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     })
 
-    async function fetchStatusPage(){
+    async function fetchStatusPage(bail, tryNumber){
+      console.log(tryNumber);
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
     }
