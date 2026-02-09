e2c91fec1c6464800990a22ef8b6c0c74a9e952f
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2026-02-09 08:17:38 +0000

    test: refactor of migration tests

--- tests/integration/integration/api/v1/migrations/other_requests.test.js
+++ tests/integration/integration/api/v1/migrations/other_requests.test.js
@@ -4,31 +4,22 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("Post to /api/v1/migrations should return 201", async () => {
-  console.log(
-    "Test desc.: Post to /api/v1/migrations with any method beside GET & POST should return 201",
-  );
+describe("Non-Permitted Method /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Using DELETE Method", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
 
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+      expect(response.status).toBe(405);
+    });
+
+    test("Using PUT Method", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
+
+      expect(response.status).toBe(405);
+    });
   });
-  expect(response1.status).toBe(405);
-
-  // const response1Body = await response1.json();
-  // console.log(response1Body);
-
-  // expect(Array.isArray(response1Body)).toBe(true);
-  // expect(response2Body.length).toBe(0);
-
-  // // another request, different method
-  // const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-  //   method: "PUT",
-  // });
-  // expect(response2.status).toBe(200);
-
-  // const response2Body = await response2.json();
-  // console.log(response2Body);
-
-  // expect(Array.isArray(response2Body)).toBe(true);
-  // expect(response2Body.length).toBe(0);
 });
