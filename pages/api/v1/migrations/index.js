dfee85bf23bd2763c30230b6aae870ec22edeb95
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2026-02-09 09:07:29 +0000

    feat: adds next-connect to the migrations controller. The dbClient.end() needs to be added in theend

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,53 +1,75 @@
+import { createRouter } from "next-connect";
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database.js";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
 
-export default async function migrations(request, response) {
-  const allowedRequests = new Set(["GET", "POST"]);
-  if (!allowedRequests.has(request.method)) {
-    return response.status(405).json({
-      error: `Method "${request.method}" not allowed`,
-    });
-  }
+const router = createRouter();
 
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.log("\nErro dentro do catch do next-connect:\n");
+  console.log(publicErrorObject);
+  response.status(500).json(publicErrorObject);
+}
+
+async function getHandler(request, response) {
   let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
 
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      await dbClient.end();
-      return response.status(200).json(pendingMigrations);
-    } else if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dryRun: false,
-      });
+  dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
-      await dbClient.end();
+  const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+  await dbClient.end();
+  return response.status(200).json(pendingMigrations);
+}
 
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
+async function postHandler(request, response) {
+  let dbClient;
 
-      return response.status(200).json(migratedMigrations);
-    } else {
-      return response.status(405).json({
-        error: `Method "${request.method}" not allowed`,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await dbClient.end();
+  dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  const migratedMigrations = await migrationRunner({
+    ...defaultMigrationOptions,
+    dryRun: false,
+  });
+
+  await dbClient.end();
+
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+
+  return response.status(200).json(migratedMigrations);
 }
--- tests/integration/integration/api/v1/migrations/other_requests.test.js
+++ tests/integration/integration/api/v1/migrations/other_requests.test.js
@@ -12,6 +12,16 @@ describe("Non-Permitted Method /api/v1/migrations", () => {
       });
 
       expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para este endpoint.",
+        action:
+          "Verifique se o método HTTP enviado é válido para este endpoint.",
+        statusCode: 405,
+      });
     });
 
     test("Using PUT Method", async () => {
