fca65a6ca0ef9c46ea105cfde17f3ea4ee205c00
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2026-03-09 08:56:52 +0000

    refactor: create model `authentication`

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,8 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
-import user from "models/user.js";
 import { UnauthorizedError } from "infra/errors.js";
-import password from "models/password";
+import authentication from "models/authentication.js";
 
 const router = createRouter();
 
@@ -13,24 +12,10 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
 
-  try {
-    const storedUser = await user.findOneByEmail(userInputValues.email);
-    const correctPasswordMatch = await password.compare(
-      userInputValues.password,
-      storedUser.password,
-    );
-    if (!correctPasswordMatch) {
-      throw new UnauthorizedError({
-        message: "Senha não confere.",
-        action: "Verifique se este dado está.",
-      });
-    }
-  } catch (error) {
-    throw new UnauthorizedError({
-      message: "Dados de autenticação não conferem.",
-      action: "Verifique se os dados enviados estão corretos.",
-    });
-  }
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInputValues.email,
+    userInputValues.password,
+  );
 
   return response.status(201).json({});
 }
