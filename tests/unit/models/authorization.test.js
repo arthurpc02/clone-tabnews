4f4ec438fcd29b0191f69dcf28e8b146365435b8
Arthur P. Cortes <arthurpc02@gmail.com>
2026-05-07 00:56:29 +0000

    test: fix ISO date format in `authorization` unit tests

--- tests/unit/models/authorization.test.js
+++ tests/unit/models/authorization.test.js
@@ -73,8 +73,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
         email: "resource@resource.com",
         password: "passswwwwd",
       };
@@ -89,8 +89,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
       });
     });
   });
