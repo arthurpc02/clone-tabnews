6df682ef092e964de477f409652bff2376918553
Arthur P. Cortes <arthurpc02@gmail.com>
2026-07-11 21:07:46 +0000

    refactor: just compacted the createRouter() part from next-connect

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,13 +4,11 @@ import controller from "infra/controller.js";
 import migrator from "models/migrator";
 import authorization from "models/authorization";
 
-const router = createRouter();
-
-router.use(controller.injectAnonymousOrUser);
-router.get(controller.canRequest("read:migrations"), getHandler);
-router.post(controller.canRequest("create:migrations"), postHandler);
-
-export default router.handler(controller.errorHandlers);
+export default createRouter()
+  .use(controller.injectAnonymousOrUser)
+  .get(controller.canRequest("read:migrations"), getHandler)
+  .post(controller.canRequest("create:migrations"), postHandler)
+  .handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const userTryingToGet = request.context.user;
--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -4,12 +4,10 @@ import controller from "infra/controller.js";
 import user from "models/user.js";
 import authorization from "models/authorization.js";
 
-const router = createRouter();
-
-router.use(controller.injectAnonymousOrUser);
-router.post(controller.canRequest("create:user"), postHandler);
-
-export default router.handler(controller.errorHandlers);
+export default createRouter()
+  .use(controller.injectAnonymousOrUser)
+  .post(controller.canRequest("create:user"), postHandler)
+  .handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
   const userInputValues = request.body;
