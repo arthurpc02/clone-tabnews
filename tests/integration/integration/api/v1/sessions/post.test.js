11710a5aeff719a2f1b027f4dad251dd35023f75
Arthur P. Cortes <arthurpc02@gmail.com>
2026-07-12 01:48:22 +0000

    test: tolerate small timestamp drift in `POST /api/v1/sessions` tests

--- tests/integration/integration/api/v1/sessions/post.test.js
+++ tests/integration/integration/api/v1/sessions/post.test.js
@@ -131,10 +131,13 @@ describe("POST /api/v1/users", () => {
       const expiresAt = new Date(responseBody.expires_at);
       const createdAt = new Date(responseBody.created_at);
 
-      expiresAt.setMilliseconds(0);
-      createdAt.setMilliseconds(0);
+      expect(expiresAt >= createdAt).toBe(true);
 
-      expect(expiresAt - createdAt).toBe(session.EXPIRATION_IN_MILLISECONDS);
+      const actualLifetimeInMilliseconds = expiresAt - createdAt;
+      const lifetimeDifferenceInMilliseconds =
+        session.EXPIRATION_IN_MILLISECONDS - actualLifetimeInMilliseconds;
+
+      expect(lifetimeDifferenceInMilliseconds).toBeLessThanOrEqual(5000);
 
       const parsedSetCookie = setCookieParser(response, {
         map: true,
